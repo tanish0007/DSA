5577cbf5b6ad2236dc611e4ad5fb811fca8c5444
tanish0007 <jangratanish0007@gmail.com>
2025-08-24 13:01:15 +0530

    simple linked list implemented successfully

--- Linked-Lists/simpleLL.cpp
+++ Linked-Lists/simpleLL.cpp
@@ -54,14 +54,14 @@ class LinkedList {
                 return;
             }
             Node* PTR = START;
-            while(PTR->NEXT->NEXT == nullptr)
+            while(PTR->NEXT->NEXT != nullptr)
                 PTR = PTR->NEXT;
-            delete PTR;
-            PTR = nullptr;
+            delete PTR->NEXT;
+            PTR->NEXT = nullptr;
         }
 
         void deleteFromPosition(int pos) {
-            if(START == nullptr || pos < 0){
+            if(START == nullptr || pos <= 0){
                 cout << "Invalid Operation. Either list is empty or position is mistyped" << endl;
                 return;
             }
@@ -90,16 +90,93 @@ class LinkedList {
             }
             Node* PTR = START;
             cout << "Linked List : ";
-            while(PTR != nullptr){
+            while(PTR != nullptr) {
                 cout << PTR->DATA << " -> ";
+                PTR = PTR->NEXT;
             }
-            cout << endl;
+            cout << "NULL" << endl;
         }
 };
 
 int main() {
     LinkedList List;
+    int choice, value;
 
-    // Pending
+    while(true) {
+        cout << endl << "1: Insertion in List" << endl << "2: Deletion in List" << endl << "3: Traverse the List" << endl << "4: EXIT" << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                Insert : cout << "Enter the value to insert: ";
+                cin >> value;
+                cout << "Where do you want to insert " << value << " ?" << endl;
+                cout << "1: Insert at Begin" << endl << "2: Insert at End" << endl << "3: Insert after a specific element" << endl;
+                cout << "Enter choice: ";
+                cin >> choice;
+
+                switch(choice) {
+                    case 1:
+                        List.insertAtBegin(value);
+                        cout << value << " inserted in beginning successfully" << endl;
+                        break;
+
+                    case 2:
+                        List.insertAtEnd(value);
+                        cout << value << " inserted in end successfully" << endl;
+                        break;
+
+                    default:
+                        cout << "Invalid insertion choice" << endl;
+                        goto Insert; 
+                        break;                       
+                }
+                break;
+
+            case 2:
+                Delete : cout << "From where you want to delete ?";
+                cout << "1: Delete first element" << endl << "2: Delete last element" << endl << "3: Delete specific element" << endl;
+                cout << "Enter choice: ";
+                cin >> choice;
+                
+                switch (choice) {
+                    case 1:
+                        List.deleteFromBegin();
+                        break;
+
+                    case 2:
+                        List.deleteFromEnd();
+                        break;
+
+                    case 3:
+                        int pos;
+                        cout << "Enter the position of specific element";
+                        cin >> pos;
+                        List.deleteFromPosition(pos);
+                        break;
+
+                    default:
+                        cout << "Invalid deletion choice" << endl;
+                        goto Delete;
+                        break;
+                }
+                break;
+
+            case 3:
+                List.traverse();
+                break;
+
+            case 4:
+                cout << "Exiting..." << endl;
+                exit(0);
+                break;
+
+            default:
+                cout << "Invalid choice" << endl;
+                break;
+        }
+    }
+
     return 0;
 }
